64c3304456ca78c6eabd2869ec1151bfaee0f4c4
ziaqian <128562280+ziaqian@users.noreply.github.com>
2023-06-11 05:32:25 +0800

    Update 1_🏠_Home.py

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -1,92 +1,84 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title="Liver Predictor",page_icon="🧑‍⚕️")
+st.set_page_config(page_title="ASD Predictor",page_icon="🧑‍⚕️")
 #Function to display the content in the Home Page
-st.title("ABOUT US")
+st.title("Homepage")
 st.sidebar.success("Select a page above")
 
 st.text("")
-st.subheader("BLues CLues")
+st.subheader("Introduction")
 st.write(
  """
- BLues CLues is a group of 5 students who are taking the subject of Machine Learning in University of Malaya.
+ Welcome to the Autism Spectrum Disorder(ASD) Predictor Project homepage! 
  
-The name BLues CLues is inspired by the TV show Blue's Clues.The show follows an animated blue-spotted dog named Blue as she leaves a trail of clues/paw prints for the host and the viewers to figure out her plans for the day.
-
+ Our ASD Predictor application is an innovative tool designed to assist in the prediction of Autism Spectrum Disorder (ASD) in adults. We understand the importance of early detection and intervention to improve the well-being and quality of life for individuals with ASD.
+ 
+ We have conducted extensive research and analysis to develop a robust prediction model for the public. Our findings are based on comprehensive data analysis, including behavioral characteristics, communication patterns, social interactions, and other relevant factors associated with ASD.
  """
 )
 
-
-st.write(
- """
- Therefore, our aim is to solve puzzles based on the clues there are.
- 
- In this project, BLues CLues has created a Liver Disease Predictor. Below are the problem statement, objectives, limitations and group members of the project:
- """
-)
 st.text("")
 st.write("---")
 st.text("")
 st.subheader("Problem Statement")
 st.write(
  """
-Overall, about 1 in 10 Americans (30 million in total) have some type of liver disease. About 5.5 million people in the U.S. have chronic liver disease or cirrhosis.
+ Autism Spectrum Disorder (ASD) affects a significant number of individuals, with an estimated prevalence of 1 in 54 children in the United States. However, many cases of ASD go undiagnosed until later in life, leading to delayed intervention and support.
 
-Some types of liver disease are becoming more common in the U.S. because they are related to rising rates of obesity. An estimated 20% to 30% of adults have excess fat in their liver, a condition called non-alcohol rekated fatty liver disease (NAFD). This may be renamed metabolic-associated fatty liver disease (MAFLD) to reflect its relationship to metabolic syndrome and conditions like diabetes, high blood pressure, high cholesterol and obesity.
+ Early detection and diagnosis of ASD in adulthood are crucial for individuals to receive appropriate interventions, therapies, and support services. However, there is a need to improve the identification and prediction of ASD, as it can be challenging to recognize the symptoms and distinguish them from other conditions.
  
-Therefore, our problem statement is how can we use machine learning to help in predicting liver disease for early detection.
+ Therefore, our problem statement is how can we use machine learning to help in predicting ASD for early detection.
+ """   
+)
+
+st.text("")
+st.write("---")
+st.text("")
+st.subheader("Goal")
+st.write(
+ """
+ * Diagnosis support: Provide individuals and healthcare professionals with a reliable tool to aid in the identification and referral process.
+
+ * Early detection: Early detection can help in improving the quality of life of individuals with ASD. 
+ 
+ * Data analysis: Presents data on the realationship between different variables, provide a glimpse into the patterns and trends of ASD.
+ 
+ * Improve Access to Services: To connect individuals with ASD to specialized services and support networks.
  """   
 )
 st.text("")
 st.write("---")
 st.text("")
-st.subheader("Objective")
+st.header("Limitations")
 st.write(
  """
- * Diagnosis support: To provide a tool for professionals to verify their diagnoses and reduce errors in diagnosis.
-
- * Early detection: Early detection can help in preventing the progression of the disease and increase recovery chance. 
+ * Our prediction model relies on the available data and variables utilized in the analysis. However, it is important to note that there may be additional significant factors not accounted for in the analysis, potentially impacting the accuracy of the predictions.
+  
+ * The apllication should be viewed as a screening tool and not a diagnostic tool. It can provide insights and indications regarding the likelihood of ASD, but it cannot replace a comprehensive evaluation conducted by qualified healthcare professionals for a definitive diagnosis.
  
- * To assist in the diagnosis of complex and confusing medical data by providing an easy-to-use and accurate tool.
- """   
-)
-st.text("")
-st.write("---")
-st.text("")
-st.header("Limitation of the Model")
-st.write(
- """
-  * The prediction model provides a binary result indicating whether or not a patient is likely to have liver disease based on the available data and variables. However, it is important to note that it does not provide a probability of getting liver disease. For further confirmation, extra tests need to be conducted if needed. 
-  
-  * The prediction model is based on the available data and variables used in the analysis. There may be other important factors that were not included in the analysis, which could affect the accuracy of the prediction.
-  
-  * The website does not provide a diagnosis or treatment plan. The treatment plan should be executed based on patients' bodies' condition in real life and their medical history. Professional advice from medical professional is needed to treat the disease. 
-  
-  * It is important to note that this website is used as a tool to supplement and not replace medical advice from medical professional. It should not be considered as a substitute for medical advice or consultation with a health care provider for proper evaluation and diagnosis.
-  
-  * This website does not provided a comprehensive assessment of a person's overall health as overall health does not solely based on the condition of liver.
-
  """
 )
 st.text("")
 st.write("---")
 st.text("")
 st.subheader("Our Team")
+    
 st.write(
  """
- * Adeline Kong Earn Ning
+ * AKMAL HAZIQ BIN SHARIP
 
- * Lee Zia Qian
+ * CARMEN LAM KAH MAN
 
- * Low Hui Yi
+ * DERNICE LEE TIAN YI
 
- * Tang Yuen Yin
+ * ESTHER LOH YUN HAN
 
- * Yoong Jing Yi 
+ * LEE ZIA QIAN
  """   
 )
 st.text("")
 st.write("---")
 st.text("")
-st.markdown("<h2 style='text-align: center; color: grey;'>\"Clues bring us one step closer to the Truth\"</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: grey;'>\"Empowering Early Intervention, Unleashing Potential\"</h2>", unsafe_allow_html=True)
+
