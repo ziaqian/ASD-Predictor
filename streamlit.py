b83ac3a9f5bba822becd1d6a26ba87c044445a0a
ziaqian <128562280+ziaqian@users.noreply.github.com>
2023-06-11 04:37:17 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -134,14 +134,16 @@ def display_prediction():
     a9 = st.selectbox("I find it easy to work out what someone is thinking or feeling just by looking at their face.", [0, 1])
     a10 = st.selectbox("I find it difficult to work out people’s intentions.", [0, 1])
 
-    # Calculate total score
-    total_score = a1 + (1 - a2) + (1 - a3) + (1 - a4) + (1 - a5) + (1 - a6) + a7 + a8 + (1 - a9) + a10
+    # Add a button to predict
+    if st.button("Predict"):
+        # Calculate total score
+        total_score = a1 + (1 - a2) + (1 - a3) + (1 - a4) + (1 - a5) + (1 - a6) + a7 + a8 + (1 - a9) + a10
 
-    # Determine prediction result
-    if total_score >= 6:
-        st.write("The patient has autism.")
-    else:
-        st.write("The patient does not have autism.")
+        # Determine prediction result
+        if total_score >= 6:
+            st.write("The patient has autism.")
+        else:
+            st.write("The patient does not have autism.")
 
 def display_other_information():
     st.title('Other Information')
